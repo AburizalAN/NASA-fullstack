dab3acf3aa7c06b626d7049eb09835f56636faae
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2023-05-30 17:29:43 +0700

    Update NavbarCreatePost.tsx

--- client/components/Navbar/NavbarCreatePost.tsx
+++ client/components/Navbar/NavbarCreatePost.tsx
@@ -4,7 +4,6 @@ const NavbarCreatePost = () => {
       <div className="flex py-3 px-4 items-center w-full mx-auto">
         <div className="flex-1">
           <div className="flex gap-x-5">
-            <div className="py-2 px-4 text-sm rounded-md">menu2</div>
           </div>
         </div>
         <div className="">
