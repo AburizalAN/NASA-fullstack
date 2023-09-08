e012a41d4c7aaa5d98dd3d5acf86aceaa8f6edb1
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2023-09-08 20:04:06 +0700

    Update index.tsx

--- client/components/content/Header/index.tsx
+++ client/components/content/Header/index.tsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline';
 
 const Header = () => {
   return (
-    <section className="max-w-6xl m-auto h-[500px] flex items-center">
+    <section className="max-w-6xl m-auto h-[600px] flex items-center">
       <div className="w-1/2 p-3">
         <h1 className="font-bold text-[50px] leading-[1.2] text-gray-800">
           <span>Hello World</span>
@@ -12,8 +12,10 @@ const Header = () => {
           <span>My Name is </span><span className="text-indigo-700">Aburizal Adi Nugroho</span>
         </h1>
       </div>
-      <div className="w-1/2">
-        <Spline scene="https://prod.spline.design/WYbqjhvLspBf8CGw/scene.splinecode" />
+      <div className="w-1/2 h-full flex items-center justify-center">
+        <div className="w-[576px] h-[500px]">
+          <Spline scene="https://prod.spline.design/dy5YV44ahejZvyM6/scene.splinecode" />
+        </div>
       </div>
     </section>
   )
