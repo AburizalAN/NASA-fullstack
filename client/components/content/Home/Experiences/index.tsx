a80f61b415ce66bca64f3e9f5ef52630117707da
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2023-10-04 02:00:11 +0700

    improvement: improve tampilan landing page

--- client/components/content/Home/Experiences/index.tsx
+++ client/components/content/Home/Experiences/index.tsx
@@ -2,32 +2,55 @@ import { RiLinkedinBoxFill, RiGithubFill } from "react-icons/ri";
 import Illustration2 from "../Header/Illustration2";
 
 const Experiences = () => {
-  const data = [
+  const experiences = [
     {
-      company: "Fast 8",
-      title: "Frontend Web Developer",
-      period: "2022 - Present",
-      desc: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed thisnquia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt",
+      company: "Fast 8 Group",
+      year: "Jul 2022 - Present",
+      title: "Frontend Web Developer ",
+      desc: `
+        <ul>
+          <li>
+            Develop E-Commerce app using next.js, redux saga, and next-PWA to Create and Maintenance Product Cart, Multiple Shipping, Payment Method, FlashSale Section Product, and Brand Catalogue Feature on AwalMula e-commerce Progressive Web App
+          </li>
+        </ul>
+      `,
     },
     {
       company: "Awal Mula",
-      title: "Frontend Web Developer - Freelance",
-      period: "2022",
-      desc: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed thisnquia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt"
+      year: "Nov 2021 - Jun 2022",
+      title: "Frontend Web Developer (Freelance)",
+      desc: `
+        <ul>
+          <li>
+            Develop E-Commerce app using next.js, redux saga, and next-PWA to Create and Maintenance Product Cart, Multiple Shipping, Payment Method, FlashSale Section Product, and Brand Catalogue Feature on AwalMula e-commerce Progressive Web App
+          </li>
+        </ul>
+      `,
     },
     {
-      company: "DOOgether",
-      title: "Junior Frontend Web Developer",
-      period: "2021 - 2022",
-      desc: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed thisnquia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt"
+      company: "Doogether",
+      year: "Aug 2021 - Mei 2022",
+      title: "Junior Front-End Engineer",
+      desc: `
+              <ul>
+                  <li>Develop and Maintenance Dashboard Web App for 
+                  Company Management using React, Context, Firebase, and 
+                  any other tools</li>
+                  <li>Develop and maintenance main website of our company,
+                  which cover information about our company, products,
+                  and also include booking and transaction feature to our
+                  products</li>
+                  <li>Develop and maintenance supporting and additional web
+                  App</li>
+              </ul>
+          `,
     },
     {
-      company: "Gilland Ganesha",
-      title: "Graphic Designer",
-      period: "2017 - 2021",
-      desc: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed thisnquia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt"
+      year: "Nov 2017 - Aug 2021",
+      title: "Graphic Designer at PT. Gilland Ganesha",
+      desc: `<ul><li>Working in Digital Marketing Division</li></ul>`,
     },
-  ]
+  ];
   return (
     <section className="max-w-6xl m-auto my-[80px]">
       <div className="flex">
@@ -42,7 +65,7 @@ const Experiences = () => {
             Engineer, and i hope i’ll be able to provide something useful
             for people around me with technology.
           </p>
-          <div className="mt-[2.4rem] pt-3 buttons flex gap-3">
+          <div className="mt-[2.4rem] pt-3 buttons flex gap-5">
             <a
               href="https://linkedin.com/in/aburizal-a-n"
               className="no-underline border border-indigo-800 hover:bg-indigo-800 px-5 py-1.5 text-indigo-800 hover:text-white rounded-[0.25rem] flex justify-center items-center gap-2"
@@ -66,16 +89,16 @@ const Experiences = () => {
         </div>
         <div className="w-6/12">
           <div className="flex">
-            <div className="bg-indigo-600 w-[8px] mr-12 rounded-full relative triangle"></div>
+            <div className="bg-indigo-600 w-[8px] mr-11 rounded-full relative triangle"></div>
             <div className="flex-1">
               <h2 className="mb-9 font-semibold">Experiences</h2>
               <div className="flex flex-col gap-y-6">
-                {data.map((item, i) => (
+                {experiences.map((item, i) => (
                   <div key={i} className="relative border rounded-md p-6">
                     <h4 className="font-semibold mb-2">{item.company}</h4>
                     <h2 className="font-bold mb-2 text-indigo-800">{item.title}</h2>
-                    <h4 className="font-semibold mb-2">{item.period}</h4>
-                    <p>{item.desc}</p>
+                    <h4 className="font-semibold mb-4">{item.year}</h4>
+                    <div className="experience-item-desc" dangerouslySetInnerHTML={{ __html: item.desc }}></div>
                   </div>
                 ))}
               </div>
--- client/components/content/Home/Projects/index.tsx
+++ client/components/content/Home/Projects/index.tsx
@@ -1,36 +1,81 @@
 import { RiArrowRightDoubleFill } from "react-icons/ri";
+import LandingPageImg from "@/public/images/LandingPage.png";
+import FinanceAppImg from "@/public/images/finance-app.png";
+import MakanKuyImg from "@/public/images/makan-kuy.png";
+import Image from 'next/image'
 
 const Projects = () => {
+  const data = [
+    {
+      id: 1,
+      title: "Personal Finance App",
+      description: `This is a personal finance app that I built using Next.js and Redux SAGA. 
+              It is a simple app that allows you to track your expenses and incomes. 
+              It's responsive and works on mobile devices.`,
+      image: FinanceAppImg,
+      link: "https://my-finance-app.vercel.app",
+      repo: "https://github.com/AburizalAN/Finance-App.git",
+      tech: [
+        "Typescript",
+        "Next.js",
+        "Next-PWA",
+        "Redux Saga",
+        "Material UI",
+        "Styled Components",
+        "Firebase Firestore",
+        "Google Login",
+        "Next API",
+      ],
+    },
+    {
+      id: 2,
+      title: "MakanKuy: Find Restourant App",
+      description: `MakanKuy is an app where you can find a Restourant from available Restourant List in Database. You can add the restourant you like to the bookmark/saved page. and you can access the Bookmark page when you are offline. This App is an SPA's Progressive Web App that made with pure Vanilla Javascript.`,
+      image: MakanKuyImg,
+      link: "https://makankuy-app.netlify.app/",
+      repo: "https://github.com/AburizalAN/MakanKuy-restaurant-apps.git",
+      tech: [
+        "HTML",
+        "SCSS",
+        "Vanilla Javascript",
+        "Webpack",
+        "Babel",
+        "eslint",
+        "Codeceptjs",
+      ],
+    },
+    {
+      id: 3,
+      title: "Landing Page Website",
+      description: `This is the Clean, Modern, and Professional Agency Website Template Based on latest Bootstrap 4 Framework.
+              You can use this template for any kind of purpose.`,
+      image: LandingPageImg,
+      link: "https://bravobuzz-purple.netlify.app",
+      repo: "https://github.com/AburizalAN/BravoBuzz.git",
+      tech: ["HTML", "SCSS", "Bootstrap", "Javascript"],
+    },
+  ];
   return (
     <section className="max-w-6xl m-auto">
       <h2 className="font-semibold mb-3">Projects</h2>
       <h1 className="mb-9 font-bold">{"Somethings I've Built"}</h1>
-      <div className="grid grid-cols-2 gap-6">
-        {[...Array(4)].map((item, i) => (
-          <div key={i} className="border rounded-lg overflow-hidden">
+      <div className="grid grid-cols-2 gap-6 justify-end">
+        {data.map((item, i) => (
+          <div key={i} className="border rounded-lg overflow-hidden flex flex-col">
             <div className="h-[300px] w-full bg-gray-200">
+              <Image src={item.image} alt={item.title} className="w-full h-full object-cover object-center" />
             </div>
-            <div className="p-6 bg-white">
-              <h3 className="font-bold mb-3">Personal Finance App</h3>
-              <p className="text-gray-500">{"This is a personal finance app that I built using Next.js and Redux SAGA. It is a simple app that allows you to track your expenses and incomes. It's responsive and works on mobile devices."}</p>
-              <div className="flex flex-shrink-0 flex-wrap gap-3 mt-4">
-                <div className="text-indigo-400 text-sm px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
-                  Typescript
-                </div>
-                <div className="text-indigo-400 text-sm px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
-                  Next.js
-                </div>
-                <div className="text-indigo-400 text-sm px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
-                  PWA
-                </div>
-                <div className="text-indigo-400 text-sm px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
-                  Material UI
-                </div>
-                <div className="text-indigo-400 text-sm px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
-                  Styled Components
-                </div>
+            <div className="p-6 bg-white flex flex-col flex-1">
+              <h3 className="font-bold mb-4">{item.title}</h3>
+              <p className="text-gray-500" dangerouslySetInnerHTML={{ __html: item.description }}></p>
+              <div className="flex flex-shrink-0 flex-wrap gap-3 mt-5 mb-8">
+                {item.tech.map((tech, i) => (
+                  <div key={i} className="text-indigo-400 text-xs px-3 py-1 bg-indigo-50 rounded-md border-indigo-300 border">
+                    {tech}
+                  </div>
+                ))}
               </div>
-              <div className="mt-6 flex justify-end">
+              <div className="flex justify-end mt-auto">
                 <a className="no-underline cursor-pointer font-semibold text-indigo-600 flex items-center gap-x-2">
                   <span>View Website</span>
                   <span><RiArrowRightDoubleFill /></span>
